078ca4de3e2e9be35b017f1267de58438d7db6b6
Abhishek Kumar Singh <abhishek.singh@hexagon.com>
2025-08-16 19:30:53 +0530

    Factory pattern

---
+++ Factory/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatterns.Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normally without any pattern, we might do something like the below one,
+            // The client code directly depends on concrete classes(Car, Bike, Truck)
+
+            //Car car = new Car();
+            //car.Drive();
+
+            //Bike bike = new Bike();
+            //bike.Drive();
+
+            //Truck truck = new Truck();
+            //truck.Drive();
+
+
+            // Client uses Factory instead of new, and only knows about IVehicle, not about concrete classes
+
+            IVehicle car = VehicleFactory.GetVehicle("car");
+            car.Drive();
+
+            IVehicle bike = VehicleFactory.GetVehicle("bike");
+            bike.Drive();
+
+            IVehicle truck = VehicleFactory.GetVehicle("truck");
+            truck.Drive();
+        }
+    }
+}
---
+++ Factory/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Factory
+{
+    //  Factory manages object creation logic in one place,
+    //  So the client code depends only on an interface/abstract class, not on concrete implementations.
+    internal class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string type)
+        {
+            IVehicle vehicle;
+
+            switch(type.ToUpper())
+            {
+                case "CAR":
+                    vehicle = new Car();
+                    break;
+                case "BIKE":
+                    vehicle = new Bike();
+                    break;
+                default:
+                    vehicle = new Truck();
+                    break;
+            }
+            return vehicle;
+        }
+    }
+}
